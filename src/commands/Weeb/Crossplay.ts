aa64975366b102bd15835bee9ba36797bde531a6
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2021-12-19 12:33:29 +0530

    Add files via upload

---
+++ src/commands/Weeb/Crossplay.ts
@@ -0,0 +1,33 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+import request from '../../lib/request'
+import { MessageType } from '@adiwajshing/baileys'
+// import { MessageType, Mimetype } from '@adiwajshing/baileys'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'cosplay',
+            description: `Will send you random crossplay img.`,
+            aliases: ['cp'],
+            category: 'weeb',
+            usage: `${client.config.prefix}crossplay`,
+            baseXp: 50
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        // fetch result of https://waifu.pics/api/sfw/waifu from the API using axios
+        return void M.reply( await request.buffer(`https://velgrynd.herokuapp.com/api/image/cosplay`),
+        MessageType.image,
+                    undefined,
+                    undefined,
+                    `_*Here you go 🐱🌸*_\n`,
+                    undefined
+                ).catch((reason: any) =>
+            M.reply(`✖ error. Please try again later.`))
+    }
+}
