84ff0c6da25a35d10d97411b574559ce1e71e94d
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2021-12-24 17:57:33 +0000

    Update Help1.ts

--- src/commands/General/Help1.ts
+++ src/commands/General/Help1.ts
@@ -20,7 +20,7 @@ export default class Command extends BaseCommand {
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const zerotwo = 
-			"https://c.tenor.com/7ZzPY3wgX_4AAAPo/zero-two-002.mp4";
+			"https://c.tenor.com/AQPsoa6-yx0AAAPo/bts-bangtan-boys.mp4";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: zerotwo },
