6fef696ee8bf9e0073e9a127d69b3d2f6eed45d5
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-02 19:15:57 +0530

    Update Help7.ts

--- src/commands/General/Help7.ts
+++ src/commands/General/Help7.ts
@@ -12,18 +12,18 @@ export default class Command extends BaseCommand {
 			command: "help7",
 			description: "Displays the info",
 			category: "general",
-			usage: `${client.config.prefix}zerotwo`,
+			usage: `${client.config.prefix}taehyung`,
 		        dm: true,
                         aliases: ['h7','?7','well7','menu7']
 		});
 	}
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
-		const zerotwo = 
+		const taehyung = 
 			"https://c.tenor.com/3ARUhKHamy0AAAPo/bts-%EB%B0%A9%ED%83%84.mp4";
 		return void this.client.sendMessage(
 			M.from,
-			{ url: zerotwo },
+			{ url: taehyung },
 			MessageType.video,
 			{
 				quoted: M.WAMessage,
