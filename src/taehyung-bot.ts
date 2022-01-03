157b655b17266ca77c2b274497199801e4d0a5ca
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-03 23:58:22 +0530

    Update taehyung-bot.ts

--- src/taehyung-bot.ts
+++ src/taehyung-bot.ts
@@ -14,8 +14,8 @@ import EventHandler from "./Handlers/EventHandler";
 
 if (!process.env.MONGO_URI) throw new Error("MONGO URL IS NOT PROVIDED");
 const client = new WAClient({
-	name: process.env.NAME || "zero-two",
-	session: process.env.SESSION || "zero-two",
+	name: process.env.NAME || "Taehyung-Bot",
+	session: process.env.SESSION || "Taehyung-Bot",
 	prefix: process.env.PREFIX || ":",
 	mods: (process.env.MODS || "")
 		.split(",")
