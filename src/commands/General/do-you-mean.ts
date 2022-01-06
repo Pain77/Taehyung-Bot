dce0ba9dd7017585d0c3af6519f54de553053a2f
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-06 22:25:26 +0530

    Update do-you-mean.ts

--- src/commands/General/do-you-mean.ts
+++ src/commands/General/do-you-mean.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            './assets/zerotwo-mean.mp4'
+            './assets/taehyung-mean.mp4'
         ]
         let taehyung = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: taehyung }, MessageType.video, {quoted:M.WAMessage,
