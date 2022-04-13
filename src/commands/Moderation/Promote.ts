354de4fdeb66bc1869200cefcefffe5f4231db8b
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-04-13 17:41:07 +0530

    Update Promote.ts

--- src/commands/Moderation/Promote.ts
+++ src/commands/Moderation/Promote.ts
@@ -31,7 +31,7 @@ export default class Command extends BaseCommand {
             if (M.groupMetadata?.admins?.includes(user)) M.reply(`✖ Skipped *${username}* as they're already an admin`)
             else {
                 await this.client.groupMakeAdmin(M.from, [user])
-                M.reply(`👑 Successfully Promoted *${username}*`)
+                M.reply(`👑 Successfully Promoted *${username}*`),
 		    let image = await this.client.assets.get('goodbye1')
             
             if (image)
