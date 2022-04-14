aed87a3f8c91b8684136d39c87c13f885853a579
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-04-14 23:48:04 +0530

    Update bank.ts

--- src/commands/Economy/bank.ts
+++ src/commands/Economy/bank.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
 
     const buttonMessage: any = {
       contentText: `🏦 Bank | ${M.sender.username}\n\n🪙 Gold: ${result}`,
-      footerText: "🚀DREADED-BOT🚀",
+      footerText: "🚀TAEHYUNG-BOT🚀",
       buttons: buttons,
       headerType: 1,
     };
